4165a86a7ab437689042274ee5d99cfcc3a8f89b
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-06-01 12:16:16 -0600

    sample code

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,113 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Bootstrap sandbox 3</title>
+		<meta charset="UTF-8">
+
+		<!-- sets IE rendering to IE-EDGE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+		<!-- sets viewport width to device width, scaling 1:1 -->
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<!-- fontawesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- OUR CUSTOM CSS -->
+		<link rel="stylesheet" href="css/styles.css" type="text/css">
+
+		<!-- jQuery -->
+		<script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+		<title>Merri J. Zibert Professional</title>
 	</head>
-	<body>
-		hai
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header>
+				<nav class="navbar navbar-default">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Merri Zibert</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="main-nav">
+							<ul class="nav navbar-nav navbar-right nav-pills">
+								<li><a href="#">Link</a></li>
+								<li><a href="#">Link</a></li>
+								<li><a href="#">Link</a></li>
+								<li><a href="#">Link</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header>
+			<main>
+				<div class="container">
+
+					<!--jumborton-->
+					<div class="jumbotron">
+						<h1>Welcome!</h1>
+						<p>blah, blah, blah...</p>
+					</div>
+
+					<!--work/play-->
+					<div class="row">
+						<div class="col-md-3 text-center">
+							<img src="http://placekitten.com/200/200" alt="image" class="img-responsive img-thumbnail img-circle">
+						</div>
+
+						<div class="col-md-6">
+							<div id="venn">
+								<div>
+									<p>at play</p>
+								</div>
+								<div>
+									<p>at work</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-3 text-center">
+							<img src="http://placekitten.com/200/200" alt="image" class="img-responsive img-thumbnail img-circle">
+						</div>
+					</div>
+
+				</div>
+			</main>
+		</div>
+		<footer>
+			<div class="container text-center">
+				<div class="row">
+					<a href="#"><i class="fa fa-linkedin well text-center" aria-hidden="true"></i></a>
+					<a href="#"><i class="fa fa-twitter well text-center" aria-hidden="true"></i></a>
+				</div>
+				<div class="row copyright">
+					Designed by MJZ 2016
+				</div>
+			</div>
+		</footer>
 	</body>
 </html>
