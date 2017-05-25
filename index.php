f7ff40f4b1b494faac352366c79a68a87e33f279
Rochelle Lewis <rlewis37@gmail.com>
2017-05-25 09:23:10 -0600

    add instructions

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 		<!-- jQuery - required for Bootstrap Components -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
-		<!--jQuery FancyBox 3 -->
+		<!-- FancyBox 3 -->
 		<script src="js/jquery.fancybox.min.js" type="text/javascript"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
@@ -50,7 +50,9 @@
 						<div class="col-xs-12">
 							<div class="instructions">
 								<h2>Instructions</h2>
-								<p>Coming soon.</p>
+								<p>Download the <a href="http://fancyapps.com/fancybox/3/" target="_blank">FancyBox 3</a> JavaScript and CSS files, and add them to your project. Link them in your HTML &lt;head&gt; tag. The JavaScript file must be added after your jQuery link, since it requires jQuery to run.</p>
+								<p>Refer to the <a href="http://fancyapps.com/fancybox/3/docs/" target="_blank">FancyBox3 Documentation</a>, and view the HTML for the FancyBox Gallery items in this demo code. This demo features the simplest and fastest implementation.</p>
+								<p>Pay close attention to the <strong>data-fancybox</strong> and <strong>data-caption</strong> attributes in the HTML.</p>
 							</div>
 						</div>
 					</div>
@@ -65,42 +67,42 @@
 						<!-- Begin FancyBox Gallery Item -->
 						<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
 							<a href="img/cat-1.jpg" class="thumbnail" data-fancybox="gallery" data-caption="Meow!">
-								<img src="img/cat-1.jpg" alt="Kitty" class="img-responsive">
+								<img src="img/cat-1.jpg" alt="Cat One" class="img-responsive">
 							</a>
 						</div>
 
 						<!-- Begin FancyBox Gallery Item -->
 						<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
 							<a href="img/cat-2.jpg" class="thumbnail" data-fancybox="gallery" data-caption="Meow #2!">
-								<img src="img/cat-2.jpg" alt="Kitty" class="img-responsive">
+								<img src="img/cat-2.jpg" alt="Cat Two" class="img-responsive">
 							</a>
 						</div>
 
 						<!-- Begin FancyBox Gallery Item -->
 						<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
 							<a href="img/cat-3.jpg" class="thumbnail" data-fancybox="gallery" data-caption="Meow #3!">
-								<img src="img/cat-3.jpg" alt="Kitty" class="img-responsive">
+								<img src="img/cat-3.jpg" alt="Cat Three" class="img-responsive">
 							</a>
 						</div>
 
 						<!-- Begin FancyBox Gallery Item -->
 						<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
 							<a href="img/cat-4.jpg" class="thumbnail" data-fancybox="gallery" data-caption="Meow #4!">
-								<img src="img/cat-4.jpg" alt="Kitty" class="img-responsive">
+								<img src="img/cat-4.jpg" alt="Cat Four" class="img-responsive">
 							</a>
 						</div>
 
 						<!-- Begin FancyBox Gallery Item -->
 						<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
 							<a href="img/cat-5.jpg" class="thumbnail" data-fancybox="gallery" data-caption="Meow #5!">
-								<img src="img/cat-5.jpg" alt="Kitty" class="img-responsive">
+								<img src="img/cat-5.jpg" alt="Cat Five" class="img-responsive">
 							</a>
 						</div>
 
 						<!-- Begin FancyBox Gallery Item -->
 						<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
 							<a href="img/cat-6.jpg" class="thumbnail" data-fancybox="gallery" data-caption="Meow #6!">
-								<img src="img/cat-6.jpg" alt="Kitty" class="img-responsive">
+								<img src="img/cat-6.jpg" alt="Cat Six" class="img-responsive">
 							</a>
 						</div>
 
